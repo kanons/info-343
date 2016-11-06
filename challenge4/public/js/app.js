19e345e2b08614e85388793e0639617eb499efd4
Kanon Shibata <kshibata@uw.edu>
2016-11-05 19:42:15 -0700

    Add Firebase

--- challenge4/public/js/app.js
+++ challenge4/public/js/app.js
@@ -1,3 +1,9 @@
-/*
- * This file should call firebase.initializeApp({ ... }) with your app credentials.
- */
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyANEVVwOscL2Mduo3lgcovORB_XTFTZ3XA",
+    authDomain: "chat-app-3296b.firebaseapp.com",
+    databaseURL: "https://chat-app-3296b.firebaseio.com",
+    storageBucket: "chat-app-3296b.appspot.com",
+    messagingSenderId: "853124667308"
+  };
+  firebase.initializeApp(config);
